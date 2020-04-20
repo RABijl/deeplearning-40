d63dcf976aa0314e6bf2feab48c3a2df82f46211
RABijl <r.a.bijl@student.tudelft.nl>
2020-04-20 13:52:49 +0200

    tidies up some scripts:

---
+++ evaluate.sh
@@ -0,0 +1,1 @@
+python scripts/eval_cityscapes/evaluate.py --cityscapes_dir cityscapes_files/ --result_dir results/cityscapes_pix2pix/test_latest --output_dir output/ --gpu_id 0 --split val --save_output_images 0
--- scripts/invert_trainB.sh
+++
@@ -1,2 +0,0 @@
-convert "trainB/train/*.jpg" -set filename:base "%[basename]" -negate "trainB_inverted/train/%[filename:base].jpg"
-
